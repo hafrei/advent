5a682d66a9cbb8253fde1e8ac67d6300d3cf4f3b
hafrei <h.a.freiburger@gmail.com>
2018-12-08 12:31:13 -0500

    Setting up the next puzzle

---
+++ day2/run3.py
@@ -0,0 +1,17 @@
+box_ids = []
+has_two = 0
+has_three = 0
+
+def find_two(list):
+  #find the letter that occurs twice
+  return "doop"
+
+def find_three(list):
+  #find the letter that occurs three times
+  return "boop"
+
+box_ids = open('input.txt', 'r').readlines()
+
+for x in box_ids:
+  print(x)
+print("Shut it off, you")
