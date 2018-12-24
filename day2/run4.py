240c1e10b1bf99fd549d8e954a7edb68b1b22afd
hafrei <h.a.freiburger@gmail.com>
2018-12-24 18:37:13 -0500

    Learned a little bit about difflib, found the answer with it

--- day2/run4.py
+++ day2/run4.py
@@ -1,29 +1,15 @@
+from difflib import get_close_matches, Differ
 box_ids = []
-running = True
-
-def compare_id(first_id, second_id, third_id):
-  incor = [0,0]
-  #print("We have:\n{}\n{}\n{}".format(first_id,second_id,third_id))
-  for windex, s in enumerate(first_id):
-    #print("{} has an index of {}".format(s,windex))
-    #print("{} has an index of {}".format(second_id[windex],windex))
-    if s != second_id[windex]:
-      incor[0] += 1
-    if s != third_id[windex]:
-      incor[1] += 1
-  return incor
 
 with open('input.txt') as f:
-  box_ids.append(f.read().splitlines())
+  box_ids = f.read().splitlines()
 
 sorted_box_ids = sorted(box_ids)
 
-while running:
-  for dindex, x in enumerate(sorted_box_ids):
-    result = compare_id(x, sorted_box_ids[1 + dindex], sorted_box_ids[2 + dindex] )
-    if result[0] == 1:
-      print("We got {} and {}!".format(x,sorted_box_ids[dindex + 1]))
-      running = False
-    elif result[1] == 1:
-      print("We got {} and {}!".format(x,sorted_box_ids[dindex + 2]))
-      running = False
+#print(box_ids)
+print("-------------")
+
+for x in box_ids:
+  countem = get_close_matches(x,sorted_box_ids,cutoff=0.95)
+  if len(countem) > 1:
+    print(countem)
