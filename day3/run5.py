5c867e1da5523aa1bd8dd65fd28ca6caf07e6aa4
hafrei <h.a.freiburger@gmail.com>
2018-12-31 00:59:32 -0500

    Still figuring out what I want to do with this

--- day3/run5.py
+++ day3/run5.py
@@ -1,8 +1,3 @@
-# from # to @ 
-# from @ to ,
-# from , to :
-# from : to x
-# from x to end
 # elf: ?, x:y, top_left: top_right, bottom_left: bottom_right
 def get_specs(deets):
   elf = deets[deets.find("#")+1 : deets.find("@")].strip()
@@ -14,23 +9,27 @@ def get_specs(deets):
   return specs
 
 def stake_claim(idea,owner):
-  bluh = "bluh"
+  #if owner is not int:
+  bluh = ''
   return bluh
 
 plan = []
-fabric = [1000,1000]
+fabric = [['' for x in range(1000)] for y in range(1000)]
 
 with open('input.txt') as f:
   claims = f.read().splitlines()
 
-for x in claims:
-  plan.append(get_specs(x))
+plan = [get_specs(x) for x in claims]
 
 # 0   1       2       3       4
-#elf, x axis, y axis, length, width
+#elf, x axis, y axis, wide, tall
 for idea in plan:
   owner = idea[0]
-  print("idea[4] is {}, idea[5] is {}".format(idea[3],idea[4]))
+  stop_x = idea[1] + idea[3]
+  stop_y = idea[2] + idea[4]
+  #for x in range(idea[1],idea[i])
+  print("idea[3] is {}, idea[4] is {}".format(idea[3],idea[4]))
+  fabric[int(idea[3])][int(idea[4])] = owner
   #if fabric[int(idea[4]),int(idea[5])]:
   #  owner = "X"
   #stake_claim(idea,owner)
